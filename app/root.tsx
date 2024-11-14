903b03fe244a62cf5bd983ce13057eb5b4cc6b6a
Chris Lane Jones <chrislanejones@gmail.com>
2024-11-14 02:14:27 -0500

    Better Background and CSS Implemented

--- app/root.tsx
+++ app/root.tsx
@@ -57,73 +57,77 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <span className="ellipse-1"></span>
+        <span className="ellipse-2"></span>
         <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <ThemeToggle />
-            <Form
-              id="search-form"
-              onChange={(event) => {
-                const isFirstSearch = q === null;
-                submit(event.currentTarget, {
-                  replace: !isFirstSearch,
-                });
-              }}
-              role="search"
-            >
-              <input
-                aria-label="Search contacts"
-                className={searching ? "loading" : ""}
-                defaultValue={q || ""}
-                id="q"
-                name="q"
-                placeholder="Search"
-                type="search"
-              />
-              <div aria-hidden hidden={!searching} id="search-spinner" />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
+          <div className="sidebar-inner">
+            <h1>Remix Contacts</h1>
+            <div>
+              <ThemeToggle />
+              <Form
+                id="search-form"
+                onChange={(event) => {
+                  const isFirstSearch = q === null;
+                  submit(event.currentTarget, {
+                    replace: !isFirstSearch,
+                  });
+                }}
+                role="search"
+              >
+                <input
+                  aria-label="Search contacts"
+                  className={searching ? "loading" : ""}
+                  defaultValue={q || ""}
+                  id="q"
+                  name="q"
+                  placeholder="Search"
+                  type="search"
+                />
+                <div aria-hidden hidden={!searching} id="search-spinner" />
+              </Form>
+              <Form method="post">
+                <button type="submit">New</button>
+              </Form>
+            </div>
+            <nav>
+              {contacts.length ? (
+                <ul>
+                  {contacts.map(
+                    (contact: {
+                      id: string;
+                      first: string;
+                      last: string;
+                      favorite: boolean;
+                    }) => (
+                      <li key={contact.id}>
+                        <NavLink
+                          className={({ isActive, isPending }) =>
+                            isActive ? "active" : isPending ? "pending" : ""
+                          }
+                          to={`contacts/${contact.id}`}
+                        >
+                          <Link to={`contacts/${contact.id}`}>
+                            {contact.first || contact.last ? (
+                              <>
+                                {contact.first} {contact.last}
+                              </>
+                            ) : (
+                              <i>No Name</i>
+                            )}{" "}
+                            {contact.favorite ? <span>★</span> : null}
+                          </Link>
+                        </NavLink>
+                      </li>
+                    )
+                  )}
+                </ul>
+              ) : (
+                <p>
+                  <i>No contacts</i>
+                </p>
+              )}
+            </nav>
           </div>
-          <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map(
-                  (contact: {
-                    id: string;
-                    first: string;
-                    last: string;
-                    favorite: boolean;
-                  }) => (
-                    <li key={contact.id}>
-                      <NavLink
-                        className={({ isActive, isPending }) =>
-                          isActive ? "active" : isPending ? "pending" : ""
-                        }
-                        to={`contacts/${contact.id}`}
-                      >
-                        <Link to={`contacts/${contact.id}`}>
-                          {contact.first || contact.last ? (
-                            <>
-                              {contact.first} {contact.last}
-                            </>
-                          ) : (
-                            <i>No Name</i>
-                          )}{" "}
-                          {contact.favorite ? <span>★</span> : null}
-                        </Link>
-                      </NavLink>
-                    </li>
-                  )
-                )}
-              </ul>
-            ) : (
-              <p>
-                <i>No contacts</i>
-              </p>
-            )}
-          </nav>
         </div>
         <div
           className={
